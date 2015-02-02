d615593a9c9361dc1c416b364f271fa739a8f5b5
Nicholas Burka <nburka@mac.com>
2015-02-01 19:24:08 -0500

    Initial implementations of BSTNode, BSTree; makefile; setting up other files

--- FiniteSet.java
+++ FiniteSet.java
@@ -1,5 +1,9 @@
 /*
 	What properties are you testing in the data1 assignment?
+
+
+	-purity: set inputted must not be equal to set outputted (==)
+	    --> deep copying by defensive copying in BSTNode, BSTree constructors
 */
 
 public class FiniteSet {
